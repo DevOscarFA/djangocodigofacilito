21251c3d4fb15ecbf2353d91eef7a7b69009c59b
Oscar Fernandez Alzate <oscarfdzalz@gmail.com>
2018-12-10 21:40:21 -0500

    Se añade opcion de solicitud de login obligatorio

--- webapp/apps/adopcion/urls.py
+++ webapp/apps/adopcion/urls.py
@@ -1,16 +1,17 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
 
 from apps.adopcion.views import index, PersonaList, PersonaCrate, PersonaUpdate, PersonaDelete, SolicitidListar, \
     SolicitudCreate, SolicitudUpdate, SolicitudDelete
 
 urlpatterns = [
     url(r'^$', index),
-    url(r'^listar$', PersonaList.as_view(), name='adopcion_listar'),
-    url(r'^crear$', PersonaCrate.as_view(), name='adopcion_crear'),
-    url(r'^editar/(?P<pk>\d+)/$', PersonaUpdate.as_view(), name='adopcion_editar'),
-    url(r'^eliminar/(?P<pk>\d+)/$', PersonaDelete.as_view(), name='adopcion_eliminar'),
-    url(r'^solicitud/listar$', SolicitidListar.as_view(), name='solicitud_listar'),
-    url(r'^solicitud/crear$', SolicitudCreate.as_view(), name='solicitud_crear'),
-    url(r'^solicitud/editar/(?P<pk>\d+)/$', SolicitudUpdate.as_view(), name='solicitud_editar'),
-    url(r'^solicitud/eliminar/(?P<pk>\d+)/$', SolicitudDelete.as_view(), name='solicitud_eliminar'),
+    url(r'^listar$', login_required(PersonaList.as_view()), name='adopcion_listar'),
+    url(r'^crear$', login_required(PersonaCrate.as_view()), name='adopcion_crear'),
+    url(r'^editar/(?P<pk>\d+)/$', login_required(PersonaUpdate.as_view()), name='adopcion_editar'),
+    url(r'^eliminar/(?P<pk>\d+)/$', login_required(PersonaDelete.as_view()), name='adopcion_eliminar'),
+    url(r'^solicitud/listar$', login_required(SolicitidListar.as_view()), name='solicitud_listar'),
+    url(r'^solicitud/crear$', login_required(SolicitudCreate.as_view()), name='solicitud_crear'),
+    url(r'^solicitud/editar/(?P<pk>\d+)/$', login_required(SolicitudUpdate.as_view()), name='solicitud_editar'),
+    url(r'^solicitud/eliminar/(?P<pk>\d+)/$', login_required(SolicitudDelete.as_view()), name='solicitud_eliminar'),
 ]
